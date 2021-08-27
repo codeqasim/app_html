768baa7b1201bdd69834fbddb5e8853561f825fc
Qasim Hussain <44278286+qaxim@users.noreply.github.com>
2021-08-27 09:53:09 +0500

    i

--- app.js
+++ app.js
@@ -1,79 +1,91 @@
-   var config = {
-   api_url: "https://api.kharidar.co/api/v1/", /* make sure the url should end with slash " / " */
-   appname: "Kharidar", 
-   language: "en", 
-   currency: "PKR",
-   logo: "http://api.kharidar.co/public/assets/img/logo.png"
-   }
+    var config = {
+    api_url: "https://api.kharidar.co/api/v1/", /* make sure the url should end with slash " / " */
+    appname: "Kharidar", 
+    language: "en", 
+    currency: "PKR",
+    logo: "http://api.kharidar.co/public/assets/img/logo.png"
+    }
 
-   // trigger async function
-   // log response or catch error of fetch promise
+    // trigger async function
+    // log response or catch error of fetch promise
 
-   // document.getElementById('app').innerHTML = data.app.appname;
-   // document.getElementById("logo").src = config.logo;
+    // document.getElementById('app').innerHTML = data.app.appname;
+    // document.getElementById("logo").src = config.logo;
 
-   // appname = sessionStorage.app.appname;
-   // sessionStorage.setItem('app', JSON.stringify(response))
+    // appname = sessionStorage.app.appname;
+    // sessionStorage.setItem('app', JSON.stringify(response))
 
-   const categories = JSON.parse(sessionStorage.getItem("categories"));
 
-   // variable is undefined or null
-   if (typeof categories === 'undefined' || categories === null) {
+    // function for loaded categories
+    function show_categories() {
 
-   // Rainbow Giant Styled Text
-   const style = 'font-weight: bold; font-size: 14px;';
-   console.log('%c Loading Categories', style);
+    // categories loop
+    const app = document.querySelector('#categories');
 
-   async function fetchAsync () {
+    categories.forEach((item) => {
+    app.innerHTML += `
+    <li>
+    <a href="${item.id}">${item.name}</a>
+    </li>
+    `;
+    });
 
-   // await response of fetch call
-   let response = await fetch(config.api_url+'categories');
+    }
 
-   // only proceed once promise is resolved
+    const categories = JSON.parse(sessionStorage.getItem("categories"));
 
-   let data = await response.json();
-   // only proceed once second promise is resolved
+    // variable is undefined or null
+    if (typeof categories === 'undefined' || categories === null) {
 
-   // store to session
-   sessionStorage.setItem('categories', JSON.stringify(data.data))
+    // Rainbow Giant Styled Text
+    const style = 'font-weight: bold; font-size: 14px;';
+    console.log('%c Loading Categories', style);
 
-   return data;
+    async function fetchAsync () {
 
-   }
+    // await response of fetch call
+    let response = await fetch(config.api_url+'categories');
 
-   fetchAsync()
-   .then(data => console.log(data.data))
+    // only proceed once promise is resolved
 
-   } else {
+    let data = await response.json();
+    // only proceed once second promise is resolved
 
-   // Rainbow Giant Styled Text
-   const style = 'font-weight: bold; font-size: 14px;';
-   console.log('%c Categories Cached', style);
-   console.log(categories);
+    // store to session
+    sessionStorage.setItem('categories', JSON.stringify(data.data))
 
-   }
+    return data;
 
-   // =========================================================== //
+    }
 
-   var uri=window.location.pathname.split('/');
+    fetchAsync()
+    .then(data => console.log(data.data))
 
-   console.log(uri[2])
+    } else {
 
-   if (uri[2] == "about"){
+    // Rainbow Giant Styled Text
+    const style = 'font-weight: bold; font-size: 14px;';
+    console.log('%c Categories Cached', style);
+    console.log(categories);
+
+    show_categories();
+
+    }
+
+    // =========================================================== //
+
+    var uri=window.location.pathname.split('/');
+
+    console.log(uri[2])
+
+    if (uri[2] == "about"){
     document.getElementById('pagename').innerHTML = 'About Us';
-   }
+    }
 
-   if (uri[2] == "home"){
+    if (uri[2] == "home"){
     document.getElementById('pagename').innerHTML = 'Home';
-   }
+    }
 
-//    function printdiv(printdivname) {
-//     var headstr = "<html><head><title>Booking Details</title></head><body>";
-//     var footstr = "</body>";
-//     var newstr = document.getElementById(printdivname).innerHTML;
-//     var oldstr = document.body.innerHTML;
-//     document.body.innerHTML = headstr+newstr+footstr;
-//     window.print();
-//     document.body.innerHTML = oldstr;
-//     return false;
-// }
+
+    // include html function
+    function includeHTML(){var e,t,n,i,u;for(e=document.getElementsByTagName("*"),t=0;t<e.length;t++)if(i=(n=e[t]).getAttribute("include"))return(u=new XMLHttpRequest).onreadystatechange=function(){4==this.readyState&&(200==this.status&&(n.innerHTML=this.responseText),404==this.status&&(n.innerHTML="Page not found."),n.removeAttribute("include"),includeHTML())},u.open("GET",i,!0),void u.send()}includeHTML();
